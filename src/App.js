a392b8f0671d51e841042756083de80dc60cccb4
nicolasmarmachado <101880825+nicolasmarmachado@users.noreply.github.com>
2022-05-10 20:56:38 -0300

    Muestra ID de la compra realizada, lógica y renderizado en Form.jsx. Borrado de componentes Checkout y CreateOrder

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import {BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Cart from "./components/Cart";
 import CartContextProvider from "./components/CartContext";
-import CreateOrder from "./components/CreateOrder";
-// import CheckOut from "./components/CheckOut";
 import Form from "./components/Form";
-import CheckOut from "./components/CheckOut";
 
 export default function App() {
 
@@ -19,13 +16,11 @@ export default function App() {
         <BrowserRouter>
           <NavBar />
           <Routes>
-            {/* <Route exact path="/order" element={<CreateOrder/>} /> */}
             <Route exact path="/form" element={<Form/>} />
             <Route exact path="/" element={<ItemListContainer />} />
             <Route exact path="/categorias/:id"element={<ItemListContainer />} />
             <Route exact path="/item/:id" element={<ItemDetailContainer />} />
             <Route exact path="/cart" element={<Cart />} /> 
-            <Route exact path="/checkout" element={<CheckOut />} /> 
           </Routes>
         </BrowserRouter>
       </CartContextProvider>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,89 +1,122 @@
-import { Button } from 'bootstrap';
-import { addDoc, collection, getFirestore, serverTimestamp } from 'firebase/firestore';
-import React, { useContext, useState } from 'react'
-import { Link } from 'react-router-dom';
+import {
+	addDoc,
+	collection,
+	getFirestore,
+	serverTimestamp,
+	getDoc,
+	doc,
+} from 'firebase/firestore';
+import React, { useContext, useEffect, useState } from 'react';
 import { Cartcontext } from './CartContext';
+import "./estilos.css";
 
 export default function Form() {
-  const {cart, valorTotal}= useContext(Cartcontext);
-  const [orderId,setOrderId]=useState("");
-  const [name, setName]=useState("");
-  const [surname, setSurname]=useState("");
-  const [phone, setPhone]=useState("");
-  const [email, setEmail]=useState("");
-  console.log(name)
-  console.log(surname)
-  console.log(email)
-  console.log(phone)
-  
-  const sendOrder =()=>{
-      const orderDate = serverTimestamp();
-      const order = {
-        buyer: { name, surname, phone, email },
-        items: cart,
-        total: valorTotal,
-        date: orderDate,
-      };
-    const db = getFirestore();
-    const ventas = collection(db, "ventas");
-    addDoc(ventas, order).then(({ id }) => setOrderId(id));
-  }
-console.log("Id: " + {orderId})
- 
-  return (
+	const { cart, valorTotal } = useContext(Cartcontext);
+	const [orderId, setOrderId] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+	const [order, setOrder] = useState(null);
+	const [name, setName] = useState('');
+	const [surname, setSurname] = useState('');
+	const [phone, setPhone] = useState('');
+	const [email, setEmail] = useState('');
+
+	const sendOrder = () => {
+		setIsLoading(true);
+		const orderDate = serverTimestamp();
+		const order = {
+			buyer: { name, surname, phone, email },
+			items: cart,
+			total: valorTotal,
+			date: orderDate,
+		};
+		const db = getFirestore();
+		const ventas = collection(db, 'ventas');
+		addDoc(ventas, order).then(({ id }) => {
+			const db = getFirestore();
+
+			const orderRef = doc(db, 'productos', id);
+
+			getDoc(orderRef).then((res) => {
+				setOrder(res);
+				setOrderId(res.id);
+				setIsLoading(false);
+			});
+		});
+	};
+
+	if (isLoading) {
+		return <h1>Cargando...</h1>;
+	}
+
+	return (
     <>
-      <div >
-        <div>
-          <label for="name">Nombre: </label>
-          <input
-            placeholder="Escriba su nombre"
-            type={"text"}
-            value={name}
-            id="name"
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <label for="surname">Apellido: </label>
-          <input
-            placeholder="Escriba su apellido"
-            type={"text"}
-            value={surname}
-            id="surname"
-            onChange={(e) => {
-              setSurname(e.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <label for="email">Email: </label>
-          <input
-            placeholder="Escriba su email"
-            type={"text"}
-            value={email}
-            id="email"
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <label for="phone">Teléfono: </label>
-          <input
-            placeholder="Escriba su teléfono"
-            type={"text"}
-            value={phone}
-            id="phone"
-            onChange={(e) => {
-              setPhone(e.target.value);
-            }}
-          />
-        </div>
-      </div>
-      <Link to='/checkout'><button type='submit' onClick={() => sendOrder()}>Submit</button></Link>
-      
-      </>
+      {order ? (
+        <>
+          <div className="checkout">
+            <b>
+              Gracias por su compra!! Esperamos que haya sido placentero...<br />
+              Su orden de compra ha sido generada con el siguiente ID: <p>{orderId}.</p><br />
+              Utilicelo para realizar el pago o para futuros reclamos. Recuerde que cuenta con 30 días para hacerlo.
+            </b>
+          </div>
+        </>
+      ) : (
+        <>
+          <div>
+            <div>
+              <label for="name">Nombre: </label>
+              <input
+                placeholder="Escriba su nombre"
+                type={"text"}
+                value={name}
+                id="name"
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label for="surname">Apellido: </label>
+              <input
+                placeholder="Escriba su apellido"
+                type={"text"}
+                value={surname}
+                id="surname"
+                onChange={(e) => {
+                  setSurname(e.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label for="email">Email: </label>
+              <input
+                placeholder="Escriba su email"
+                type={"text"}
+                value={email}
+                id="email"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label for="phone">Teléfono: </label>
+              <input
+                placeholder="Escriba su teléfono"
+                type={"text"}
+                value={phone}
+                id="phone"
+                onChange={(e) => {
+                  setPhone(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+          <button type="submit" onClick={() => sendOrder()}>
+            Submit
+          </button>
+        </>
+      )}
+    </>
   );
 }
